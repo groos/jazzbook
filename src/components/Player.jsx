4c2a5e8166dd67631bcb6346c095eacefee15b89
Nick Groos <ntgroos@gmail.com>
2019-07-30 14:05:22 -0500

    changed tonejs code to use scheduling

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,37 +7,42 @@ export default class Player extends React.Component {
     constructor(props) {
         super(props);
 
-        this.playMeasures = this.playMeasures.bind(this);
-        this.stopPlay = this.stopPlay.bind(this);
-    }
-
-    stopPlay = () => {
-        this.state.synth.disconnect();
-        this.state.synth.dispose();
-    }
-
-    playMeasures = () => {
-        
-        this.setState({
+        this.state = {
             synth: new Tone.Synth().toMaster()
-        }, () => {
-            Tone.Transport.bpm.value = parseInt(this.props.beatsPerMinute);
+        };
 
-            var measureCount = 0;
-    
-            this.props.measures.forEach((measure) => {
-                this.props.updateActiveMeasure(measureCount++);
-                var beatCount = 0;
-    
-                getArpeggioNotes(measure.note, measure.chordType).forEach((note) => {
-                    var playNoteAt = measureCount + ':' + beatCount;
-                    var noteDuration = '4n'
-    
-                    console.log('Play ' + note + ' for ' + noteDuration + ' at ' + playNoteAt);
-                    this.state.synth.triggerAttackRelease(note, noteDuration, playNoteAt);
-    
-                    beatCount++;
-                });
+        this.play = this.play.bind(this);
+        this.schedulePlayback = this.schedulePlayback.bind(this);
+    }
+
+    componentDidMount() {
+        this.schedulePlayback();
+    }
+
+    play = () => Tone.Transport.toggle();
+
+    schedulePlayback = () => {
+        Tone.Transport.bpm.value = parseInt(this.props.beatsPerMinute);
+
+        var measureCount = 0;
+
+        this.props.measures.forEach((measure) => {
+            measureCount++;
+            var beatCount = 0;
+
+            getArpeggioNotes(measure.note, measure.chordType).forEach((note) => {
+                var currentMeasure = measureCount - 1;
+
+                // ToneJS notation for keeping time
+                var playNoteAt = measureCount + ':' + beatCount;
+                var noteDuration = '4n'
+
+                Tone.Transport.schedule((time) => {
+                    this.props.updateActiveMeasure(currentMeasure);
+                    this.state.synth.triggerAttackRelease(note, noteDuration, time);
+                }, playNoteAt)
+
+                beatCount++;
             });
         });
     }
@@ -45,8 +50,7 @@ export default class Player extends React.Component {
     render () {
         return <div className="player-wrapper">
             <h5>Player</h5>
-            <button onClick={this.playMeasures}>Play!</button>
-            <button onClick={this.stopPlay}>Stop</button>
+            <button onClick={this.play}>Start/Stop!</button>
         </div>
     }
 }
